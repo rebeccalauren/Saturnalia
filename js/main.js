2e3d74586d71be738ee984784706d4b90496d1f7
Rebecca Lauren <rebeccalauren@gmail.com>
2016-05-31 21:25:17 -0400

    Working jquery columns, favicon position, blog content added with book

--- js/main.js
+++ js/main.js
@@ -1,67 +1,70 @@
-$(document).on("ready", function(){
-	var rightHeight = document.getElementById('right1').clientHeight;
-	var leftHeight = document.getElementById('left1').clientHeight;
-if (leftHeight > rightHeight) {
-	document.getElementById('right1').style.height=leftHeight+'px';
-} else {
-	document.getElementById('left1').style.height=rightHeight+'px';
+ $(document).on("ready", function(){
+		var rightHeight = $("#right1").outerHeight(true);
+		var leftHeight = $("#left1").outerHeight(true);
+		var rightHeight2 = $("#right2").outerHeight(true);
+		var leftHeight2 = $("#left2").outerHeight(true);
+		var rightHeight3 = $("#right3").outerHeight(true);
+		var leftHeight3 = $("#left3").outerHeight(true);
+
+if (rightHeight > leftHeight) {
+	$("#left1").css("height", rightHeight + "px");
+}
+else {
+	$("#right1").css("height", leftHeight + "px");
 }
 
-var rightHeight2 = document.getElementById('right2').clientHeight;
-	var leftHeight2 = document.getElementById('left2').clientHeight;
 if (leftHeight2 > rightHeight2) {
-	document.getElementById('right2').style.height=leftHeight2+'px';
-} else {
-	document.getElementById('left2').style.height=rightHeight2+'px';
+	$("#right2").css("height", leftHeight2 + "px");
 }
-
-var rightHeight3 = document.getElementById('right3').clientHeight;
-	var leftHeight3 = document.getElementById('left3').clientHeight;
-if (leftHeight3 > rightHeight3) {
-	document.getElementById('right3').style.height=leftHeight3+'px';
-} else {
-	document.getElementById('left3').style.height=rightHeight3+'px';
+else {
+	$("#left2").css("height", rightHeight2 + "px");
 }
-
-});
-
-//YOU ALMOST HAVE THE JQUERY VERSION FIGURED OUT BELOW:
-
-// $(document).on("ready", function(){
-// 		var rightHeight = $("#right1").outerHeight(true);
-// 		var leftHeight = $("#left1").outerHeight(true);
-// 		var rightHeight2 = $("#right2").outerHeight(true);
-// 		var leftHeight2 = $("#left2").outerHeight(true);
-// 		var rightHeight3 = $("#right3").outerHeight(true);
-// 		var leftHeight3 = $("#left3").outerHeight(true);
-
-// if (rightHeight > leftHeight) {
-// 	$(leftHeight).css("height", rightHeight + "px");
-// }
-// else {
-// 	$(rightHeight).css("height", leftHeight + "px");
-// }
-
-// if (rightHeight2 > leftHeight2) {
-// 	$(leftHeight2).css("height", rightHeight2 + "px");
-// }
-// else {
-// 	$(rightHeight2).css("height", leftHeight2 + "px");
-// }
 
 // if (rightHeight3 > leftHeight3) {
-// 	$(leftHeight3).css("height", rightHeight3 + "px");
+// 	$("#left3").css("height", rightHeight3 + "px");
 // }
 // else {
-// 	$(rightHeight3).css("height", leftHeight3 + "px");
+// 	$("#right3").css("height", leftHeight3 + "px");
 // }
-// });
+});
 
 // var setHeight = function(){
 // 		var divHeight = $(".authorPic").innerHeight();
 
 // 		$("#leftFirstBox").css({transform: setHeight(divHeight)});
 // 	}
+
+// $(document).on("ready", function(){
+// 	var rightHeight = document.getElementById('right1').clientHeight;
+// 	var leftHeight = document.getElementById('left1').clientHeight;
+// if (leftHeight > rightHeight) {
+// 	document.getElementById('right1').style.height=leftHeight+'px';
+// } else {
+// 	document.getElementById('left1').style.height=rightHeight+'px';
+// }
+
+// var rightHeight2 = document.getElementById('right2').clientHeight;
+// 	var leftHeight2 = document.getElementById('left2').clientHeight;
+// if (leftHeight2 > rightHeight2) {
+// 	document.getElementById('right2').style.height=leftHeight2+'px';
+// } else {
+// 	document.getElementById('left2').style.height=rightHeight2+'px';
+// }
+
+// var rightHeight3 = document.getElementById('right3').clientHeight;
+// 	var leftHeight3 = document.getElementById('left3').clientHeight;
+// if (leftHeight3 > rightHeight3) {
+// 	document.getElementById('right3').style.height=leftHeight3+'px';
+// } else {
+// 	document.getElementById('left3').style.height=rightHeight3+'px';
+// }
+
+// });
+
+
+//YOU ALMOST HAVE THE JQUERY VERSION FIGURED OUT BELOW:
+
+//
 
 // 	//Center the logo when the browser resizes
 // 	$(window).resize(setHeight);
